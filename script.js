e91515b692a26728aa9d2caab4d5751059a68ea7
slicetext <theokozak@gmail.com>
2026-08-28 15:15:25 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('#timeline-wrapper');
+
+container.addEventListener('wheel', (event) => {
+
+    update_descriptions()
+    // If the user is already scrolling horizontally, let the browser handle it
+    if (event.deltaX !== 0)  return;
+    
+    // Redirect vertical scroll to horizontal
+    // container.scrollLeft += event.deltaY;
+    container.scrollBy({ left: (window.innerWidth * 31) / 100 * Math.sign(event.deltaY), behavior: 'smooth' });
+    
+    // Prevent the default vertical page scroll
+    event.preventDefault();
+});
+
+window.addEventListener('keydown', (e) => {
+  const scrollAmount = (window.innerWidth * 30) / 100; // Adjust speed here
+  if (e.key === 'ArrowRight') {
+    container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  } else if (e.key === 'ArrowLeft') {
+    container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  }
+});
